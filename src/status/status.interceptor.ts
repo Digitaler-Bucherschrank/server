c575a8952eeaf7c077400be8ceb1cb9d47e7b221
Flawn <fl4wn@protonmail.com>
2021-11-27 16:39:13 +0100

    :sparkles: Introduce Maintenance Mode
    By changing assets/maintenance.json we'll be able to block all requests while not needing to shut down the server and hence confuse our mobile apps about 404s. Check changes for more information how to use it.

---
+++ src/status/status.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException, HttpStatus } from "@nestjs/common";
+import { Observable } from 'rxjs';
+import * as path from "path";
+import * as fs from "fs";
+import { Config } from "../config";
+
+@Injectable()
+export class StatusInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // Check if server is in maintenance mode
+    // ==> 2 Modes: "maintenance" and "normal"
+    let file = fs.readFileSync(path.join(Config.working_dir, './assets/maintenance.json'), 'utf8');
+    let maintenance = JSON.parse(file);
+
+    console.log("GG")
+
+
+    // TODO: Return approximate time for maintenance
+    if (maintenance.status === "maintenance") {
+      throw new HttpException('maintenance_mode', HttpStatus.SERVICE_UNAVAILABLE)
+    } else {
+      return next.handle();
+    }
+  }
+}
